2cf749a199d0fe4807fdb4fee61eb01ee8fcd872
roman-bessmertnyi <roman.bezsmertniy@gmail.com>
2018-11-30 12:28:58 +0200

    added linux arm drivers

--- src/main/java/donkeybug/controller/WebcamController.java
+++ src/main/java/donkeybug/controller/WebcamController.java
@@ -1,8 +1,10 @@
 package donkeybug.controller;
 
 import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.ds.v4l4j.V4l4jDriver;
 import donkeybug.model.WebcamDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -17,6 +19,9 @@ import java.io.ByteArrayOutputStream;
 @EnableScheduling
 @Controller
 public class WebcamController {
+    @Value("${OS}")
+    private String OS;
+
     Webcam webcam;
 
     @Autowired
@@ -24,21 +29,30 @@ public class WebcamController {
 
     @PostConstruct
     public void initIt() throws Exception {
+        switch (OS){
+            case "Linux":
+                Webcam.setDriver(new V4l4jDriver()); // this is important
+                break;
+        }
         // get default webcam and open it
         webcam = Webcam.getDefault();
-        webcam.setViewSize(new Dimension(640, 480));
-        webcam.open();
+        if(webcam != null) {
+            webcam.setViewSize(new Dimension(640, 480));
+            webcam.open();
+        }
     }
 
     @Scheduled(fixedRate = 80)
     public void sendImage()  throws Exception {
-        // get image
-        BufferedImage image = webcam.getImage();
+        if(webcam != null) {
+            // get image
+            BufferedImage image = webcam.getImage();
 
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ImageIO.write(image, "jpg", baos);
-        byte[] byteArray = baos.toByteArray();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(image, "jpg", baos);
+            byte[] byteArray = baos.toByteArray();
 
-        this.template.convertAndSend("/topic/webcam", new WebcamDTO(byteArray));
+            this.template.convertAndSend("/topic/webcam", new WebcamDTO(byteArray));
+        }
     }
 }
